5a0ea72814a6f949f07b43f637be91618ab72bda
Divyanshu Singh <divyanshu.avi@gmail.com>
2020-10-21 14:09:22 +0530

    Added printGrammar

--- grammar.c
+++ grammar.c
@@ -8,32 +8,59 @@ typedef struct edon
     char str[id_len];
     struct edon* next;
 }node;
-
-int main()
+typedef struct
 {
-    FILE *grammar = fopen("grammar.txt", "r");
-    if(!grammar)
+    node **rules;
+    int size;
+}grammar;
+
+void printGrammar(grammar G)
+{
+    int i;
+    for(i=0;i<G.size;i++)
     {
-        printf("Failed to open grammar.txt. Please check if the file exists.\n");
-        return 1;
+        printf("%s -> ", G.rules[i]->str);
+        node *temp = G.rules[i]->next;
+        while(temp)
+        {
+            printf("%s ", temp->str);
+            temp = temp->next;
+        }
+        printf("\n");
     }
+    printf("Grammar contains %d rules.\n", G.size);
+    
+}
+
+grammar readGrammar(const char* source)
+{
     char buffer[line_size];
     int count=0, lines=0;
     char *token;
     node *temp, *new_node;
-
-    while(fgets(buffer, line_size, grammar))
-        lines++;
-    fclose(grammar);
-    node **garr = (node**)malloc(sizeof(node*)*lines);
+    grammar G;
+    G.size = 0;
     
-    grammar = fopen("grammar.txt", "r");
-    while(fgets(buffer, line_size, grammar))
+    FILE *fp = fopen(source, "r");
+    if(!fp)
+    {
+        printf("Failed to open grammar.txt. Please check if the file exists.\n");
+        return G;
+    }
+
+    while(fgets(buffer, line_size, fp))
+        lines++;
+    fclose(fp);
+    G.size = lines;
+    G.rules = (node**)malloc(sizeof(node*)*G.size);
+    
+    fp = fopen("grammar.txt", "r");
+    while(fgets(buffer, line_size, fp))
     {
         token = strtok(buffer, " ");
         count++;
-        garr[count] = (node*)malloc(sizeof(node));
-        temp = garr[count];
+        G.rules[count] = (node*)malloc(sizeof(node));
+        temp = G.rules[count];
         temp->next = NULL;
         strcpy(temp->str, token);
         while(token = strtok(NULL, " "))
@@ -45,7 +72,15 @@ int main()
             temp = new_node;
         }
     }
+    fclose(fp);
+    return G;
+}
 
-    fclose(grammar);
+int main()
+{
+    grammar G = readGrammar("grammar.txt");
+    if(G.size == 0)
+        return 1;
+    printGrammar(G);
     return 0;
 }
