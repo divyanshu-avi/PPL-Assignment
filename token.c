6cd018d85814564fef2b7e0206c925542d738ed0
Divyanshu Singh <divyanshu.avi@gmail.com>
2020-10-23 17:12:56 +0530

    Added token info (key/id/num). Might add more token types later.

--- token.c
+++ token.c
@@ -2,12 +2,15 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<ctype.h>
 #define id_len 25
 #define line_size 100
+#define no_of_key 32//Number of keywords
+#define key_size 11//max size of a keyword
 
 typedef struct tok
 {
-    enum { key, op, punc, id, num}token;
+    enum { key, num, id}token;
     char lexeme[id_len];
     int line_no;
     struct tok* next;
@@ -64,7 +67,44 @@ void printTokenStream(tokenStream *s)
 {
     while(s)
     {
-        printf("%d-%s\n", s->line_no, s->lexeme);
+        printf("%d-%s-%d\n", s->line_no, s->lexeme, s->token);
+        s = s->next;
+    }
+}
+
+char** populateKeywords(char *src)
+{
+    char **keywords = (char**)malloc(sizeof(char*)*no_of_key);
+    char buffer[key_size];
+    FILE *fp = fopen(src, "r");
+    if(!fp)
+    {
+        printf("Failed to open %s. Please check if the file exists.\n", src);
+        return keywords;
+    }
+    for(int i = 0;i<no_of_key; i++)
+    {
+        fgets(buffer, key_size, fp);
+        keywords[i] = (char*)malloc(sizeof(char)*no_of_key);
+        strcpy(keywords[i], strtok(buffer, "\n"));
+    }
+    return keywords;
+}
+
+void setTokens(tokenStream *s, char **keywords)
+{
+    int i;
+    while(s)
+    {
+        for(i=0;i<no_of_key;i++)
+            if(!strcmp(s->lexeme, keywords[i]))
+                break;
+        if(i<no_of_key)
+            s->token = key;
+        else if(isdigit(s->lexeme[0]))
+            s->token = num;
+        else
+            s->token = id;
         s = s->next;
     }
 }
@@ -72,6 +112,8 @@ void printTokenStream(tokenStream *s)
 int main()
 {
     tokenStream *s = tokeniseSourcecode("sourcecode.txt");
+    char **keywords = populateKeywords("keywords_terminal.txt");
+    setTokens(s, keywords);
     printTokenStream(s);
     return 0;
 }
