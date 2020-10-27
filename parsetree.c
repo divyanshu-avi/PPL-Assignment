f65caa79876daa9bb44c8ce7937447522b9fc75e
Divyanshu Singh <divyanshu.avi@gmail.com>
2020-10-27 23:53:19 +0530

    bug fix in createParseTree

--- parsetree.c
+++ parsetree.c
@@ -13,9 +13,10 @@ typedef struct tnode
     int line_no;
     bool is_terminal;
     int nofc;
+    int depth;
 }tree;
 
-tree* newTreeNode(const char *str, bool is_terminal, int line_no)
+tree* newTreeNode(const char *str, bool is_terminal, int line_no, int depth)
 {
     tree *root = (tree*)malloc(sizeof(tree));
     root->left = NULL;
@@ -26,59 +27,58 @@ tree* newTreeNode(const char *str, bool is_terminal, int line_no)
     root->is_terminal = is_terminal;
     root->line_no = line_no;
     root->nofc = 0;
+    root->depth = depth;
     return root;
 }
 
 bool createParseTree(tree *root, tokenStream **s, grammar G)
 {
-    if(!s)//Reached end of token stream
+    if(!(*s))//Reached end of token stream
         return true;
     if(!root)
-        root = newTreeNode(G.rules[0]->str, false, 0);
+        root = newTreeNode(G.rules[0]->str, false, 0, 0);
 
     if(root->is_terminal)
     {
         if(!strcmp(root->str, "e"))//e means epsilon
         {
-            printf("\033[0;32m");
-            printf("Used epsilon\n", root->str, (*s)->lexeme);
-            printf("\033[0m");
             return true;
         }
         else if( ((*s)->token == key) && !strcmp((*s)->lexeme, root->str) )//keyword
         {
-            printf("\033[0;32m");
-            printf("Terminal %s matched with token %s el1\n", root->str, (*s)->lexeme);
-            printf("\033[0m");
-            (*s) = (*s)->next;
+            //printf("\033[0;32m");
+            //printf("Terminal %s matched with token %s el1\n", root->str, (*s)->lexeme);
+            //printf("\033[0m");
+            *s = (*s)->next;
             return true;
         }
         else if((*s)->token == num && !strcmp(root->str, "digit"))//integer const
         {
-            printf("\033[0;32m");
-            printf("Terminal %s matched with token %s el2\n", root->str, (*s)->lexeme);
-            printf("\033[0m");
-            (*s) = (*s)->next;
+            //printf("\033[0;32m");
+            //printf("Terminal %s matched with token %s el2\n", root->str, (*s)->lexeme);
+            //printf("\033[0m");
+            *s = (*s)->next;
+            //printf("***%s***\n", (*s)->lexeme);
             return true;
         }
         else if((*s)->token == id && !strcmp(root->str, "id"))//identifier
         {
-            printf("\033[0;32m");
-            printf("Terminal %s matched with token %s el3\n", root->str, (*s)->lexeme);
-            printf("\033[0m");
-            (*s) = (*s)->next;
+            //printf("\033[0;32m");
+            //printf("Terminal %s matched with token %s el3\n", root->str, (*s)->lexeme);
+            //printf("\033[0m");
+            *s = (*s)->next;
             return true;
         }
         else
         {
-            printf("\033[0;31m");
-            printf("Terminal %s didn't match token %s\n", root->str, (*s)->lexeme);
-            printf("\033[0m");
+            //printf("\033[0;31m");
+            //printf("Terminal %s didn't match token %s\n", root->str, (*s)->lexeme);
+            //printf("\033[0m");
             return false;
         }
     }
     //Now we have to expand the non-terminal inside root and make its children
-    int i, j, size;
+    int i, j;
     node *temp;
     tokenStream *tmpstream;
     bool flag = false;
@@ -90,17 +90,24 @@ bool createParseTree(tree *root, tokenStream **s, grammar G)
         for(temp = G.rules[i]->next; temp ; temp = temp->next)//No of children required
             root->nofc++;
         root->children = (tree**)malloc(sizeof(tree*)*root->nofc);
-        printf("Rule %d matched with %s. %d children needed\n", i, root->str, root->nofc);
+        //printf("Rule %d matched with %s. %d children needed\n", i+1, root->str, root->nofc);
         temp = G.rules[i]->next;
         tmpstream = *s;
-        for(j=0 ; j<root->nofc ; j++)
+        for(j=0 ; j<root->nofc; j++)
         {
-            root->children[j] = newTreeNode(temp->str, temp->terminal, (*s)->line_no);
+            root->children[j] = newTreeNode(temp->str, temp->terminal, (*s)->line_no, root->depth+1);
+            if(!strcmp(root->children[j]->str, "e"))
+            {
+                //printf("\033[0;32m");
+                //printf("Replaced %s with epsilon\n", G.rules[i]->str);
+                //printf("\033[0m");
+            }
             flag = createParseTree(root->children[j], s, G);
+            //printf("|||%s||| %d\n", (*s)->lexeme, flag);
             temp = temp->next;
             if(!flag)//If mismatch free all the memory allocated.
             {
-                s = &tmpstream;
+                *s = tmpstream;
                 for(j; j>=0;j--)
                     free(root->children[j]);
                 free(root->children);
@@ -109,8 +116,10 @@ bool createParseTree(tree *root, tokenStream **s, grammar G)
             }
         }
     }
-    if(!flag)
-        printf("No rule matched for %s\n", root->str);
+    //if(!flag)
+        //printf("No rule matched for %s\n", root->str);
+    //if(flag && (*s))
+       // printf("Returning from %s with tokenstream at %s line %d\n", root->str, (*s)->lexeme, (*s)->line_no);
     return flag;
 }
 
@@ -118,7 +127,8 @@ void printParseTree(tree *root)
 {
     if(!root)
         return;
-    printf("%s\n", root->str);
+    char *temp = root->is_terminal?"Yes":"No";
+    printf("%20s | %8s | %5d\n", root->str, temp, root->depth);
     for(int i = 0; i<root->nofc; i++)
         printParseTree(root->children[i]);
 }
@@ -127,9 +137,20 @@ int main()
 {
     tokenStream *s = tokeniseSourcecode("sourcecode.txt");
     grammar G = readGrammar("grammar.txt");
-    tree *root = newTreeNode(G.rules[0]->str, false, 0);
+    tree *root = newTreeNode(G.rules[0]->str, false, 0 ,0);
     bool flag = createParseTree(root, &s, G);
     if(!flag)
-        printf("Something went horribly wrong!\n");
+    {
+        printf("\033[0;31m");
+        printf("Something went horribly wrong while creating the parse tree!\n");
+        printf("\033[0m");
+        return 1;
+    }
+    printf("\033[0;32m");
+    printf("Parse tree is created successfully!\n\n");
+    printf("\033[0m");
+
+    printf("Printing the parse tree\n\n");
+    printf("%20s | %8s | %5s\n", "Symbol", "Terminal", "Depth");
     printParseTree(root);
 }
